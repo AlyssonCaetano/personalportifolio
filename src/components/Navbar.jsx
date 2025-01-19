eeb1729b057ff57fa07d803cf710858e119941ff
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2025-01-19 12:48:33 -0300

    feat:creat componenmt Navbar and styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,22 @@
-
+import "react";
 
 const Navbar =() => {
+    
+
     return (
-        <div>
-            <div>
-                <div>Dev | Caetano</div>
-                <div>
-                    <a href="#home">Home</a>
-                    <a href="#home">About me</a>
-                    <a href="#home">Services</a>
-                    <a href="#home">Projects</a>
-                    <a href="#home">Contact</a>
+        <nav className="bg-black text-white px-8 md:px-16 lg:px-24">
+            <div className="container py-2 flex justify-center md:justify-between items-center">
+                <div className="text-2xl font-bold hidden md:inline">Dev | Caetano</div>
+                <div className="space-x-6">
+                    <a href="#home" className="hover:text-gray-400">Home</a>
+                    <a href="#aboutme" className="hover:text-gray-400">About me</a>
+                    <a href="#services" className="hover:text-gray-400">Services</a>
+                    <a href="#projects" className="hover:text-gray-400">Projects</a>
+                    <a href="#contact" className= "hover:text-gray-400">Contact</a>
                 </div>
-                <button>Connect Me</button>
+                <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white md:inline transform transition-transform duration-300 gover:scale-105 px-4 py-2 rounded-full">Connect Me</button>
             </div>
-        </div>
+        </nav>
     )
 }
 
