0367712ca843e45b847be33a5cd5a27cd870d1e7
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2025-01-19 11:48:00 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+
+
+const Navbar =() => {
+    return (
+        <div>
+            <div>
+                <div>Dev | Caetano</div>
+                <div>
+                    <a href="#home">Home</a>
+                    <a href="#home">About me</a>
+                    <a href="#home">Services</a>
+                    <a href="#home">Projects</a>
+                    <a href="#home">Contact</a>
+                </div>
+                <button>Connect Me</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
